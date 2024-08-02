ff8f21c51be6b9584188f0273dfd150a1e4959fb
vandodev <vandoaparecido@hotmail.com>
2024-08-02 09:04:14 -0300

    :computer: :credit_card: :moneybag: Criando DTO's no nest

--- api/src/user/user.controller.ts
+++ api/src/user/user.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/createUser.dto';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    @Post()
+    async createUser(@Body() createUser: CreateUserDto) {
+        return {
+            ...createUser,
+            password: undefined,
+        };
+    }
+}
