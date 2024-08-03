412bd0662cbbc216e106c315ea593a6fe9b845e5
vandodev <vandoaparecido@hotmail.com>
2024-08-03 08:01:14 -0300

    :computer: :credit_card: :moneybag: Criptografando senha

--- api/src/user/user.service.ts
+++ api/src/user/user.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
 import { User } from './interfaces/user.interface';
+import { hash } from 'bcrypt';
 
 @Injectable()
 export class UserService {
+    
     async createUser(createUserDto: CreateUserDto): Promise<User> {
+        const saltOrRounds = 10;
+        const passwordHashed = await hash(createUserDto.password, saltOrRounds);
         return{
             ...createUserDto,
-            id:1
+            id:1,
+            password: passwordHashed,
         }
     }
 }
