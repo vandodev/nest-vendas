a8329d18bf859529995904cd13b9203b621e8743
vandodev <vandoaparecido@hotmail.com>
2024-08-03 07:52:01 -0300

    :computer: :credit_card: :moneybag: Criando interface e adicionando id

--- api/src/user/user.controller.ts
+++ api/src/user/user.controller.ts
@@ -1,13 +1,14 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
+import { UserService } from './user.service';
+import { User } from './interfaces/user.interface';
 
 @Controller('user')
 export class UserController {
+    constructor(private readonly userService: UserService) {}
+
     @Post()
-    async createUser(@Body() createUser: CreateUserDto) {
-        return {
-            ...createUser,
-            password: undefined,
-        };
+    async createUser(@Body() createUser: CreateUserDto): Promise<User> {
+        return this.userService.createUser(createUser);
     }
 }
--- api/src/user/user.service.ts
+++ api/src/user/user.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dtos/createUser.dto';
+import { User } from './interfaces/user.interface';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    async createUser(createUserDto: CreateUserDto): Promise<User> {
+        return{
+            ...createUserDto,
+            id:1
+        }
+    }
+}
