f27be83854ea5a460db89136dfcdcb2c10640e58
vandodev <vandoaparecido@hotmail.com>
2024-08-05 08:00:56 -0300

    :computer: :credit_card: :moneybag: Buscando todos os estados

--- api/src/state/state.module.ts
+++ api/src/state/state.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { StateEntity } from './entities/state.entity';
 import { StateController } from './state.controller';
 import { StateService } from './state.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([StateEntity])],
   controllers: [StateController],
   providers: [StateService]
 })
