39227aaf03d9ce46fc3293514621784257aa7538
vandodev <vandoaparecido@hotmail.com>
2024-08-05 09:16:04 -0300

    :computer: :credit_card: :moneybag: Implementando cache

--- api/src/city/city.module.ts
+++ api/src/city/city.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
 import { CityController } from './city.controller';
+import { CacheModule } from '@nestjs/cache-manager';
 import { CityService } from './city.service';
 import { CityEntity } from './entities/city.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CityEntity])],
+  imports: [
+    CacheModule.register({
+      ttl: 900000000,
+    }),
+    TypeOrmModule.forFeature([CityEntity]),
+  ],
   controllers: [CityController],
   providers: [CityService]
 })
--- api/src/city/city.service.ts
+++ api/src/city/city.service.ts
@@ -1,5 +1,6 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import {  CACHE_MANAGER, Cache } from '@nestjs/cache-manager';
 import { Repository } from 'typeorm';
 import { CityEntity } from './entities/city.entity';
 
@@ -8,13 +9,26 @@ export class CityService {
   constructor(
     @InjectRepository(CityEntity)
     private readonly cityRepository: Repository<CityEntity>,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
   ) {}
 
   async getAllCitiesByStateId(stateId: number): Promise<CityEntity[]> {
-    return this.cityRepository.find({
-        where:{
-            stateId,
-        }
-    })
+    const citiesCache: CityEntity[] = await this.cacheManager.get(
+      `state_${stateId}`,
+    );
+
+    if (citiesCache) {
+      return citiesCache;
+    }
+
+    const cities = await this.cityRepository.find({
+      where: {
+        stateId,
+      },
+    });
+
+    await this.cacheManager.set(`state_${stateId}`, cities);
+
+    return cities;
   }
 }
