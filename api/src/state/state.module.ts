d541e082ce7f6fdd811abbb6ffe8497d84b09107
vandodev <vandoaparecido@hotmail.com>
2024-08-05 07:47:33 -0300

    :computer: :credit_card: :moneybag: Criando a base de controller e services

--- api/src/state/state.module.ts
+++ api/src/state/state.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { StateController } from './state.controller';
+import { StateService } from './state.service';
 
-@Module({})
+@Module({
+  controllers: [StateController],
+  providers: [StateService]
+})
 export class StateModule {}
