c292866f61de325e3d068a62e2c8619cafedf7da
vandodev <vandoaparecido@hotmail.com>
2024-08-07 08:11:56 -0300

    :computer: :credit_card: :moneybag: Tratamento de exceção usuário não emcontrado

--- api/src/address/address.module.ts
+++ api/src/address/address.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AddressController } from './address.controller';
 import { AddressService } from './address.service';
 import { AddressEntity } from './entities/address.entity';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AddressEntity])],
+  imports: [TypeOrmModule.forFeature([AddressEntity]), UserModule],
   controllers: [AddressController],
   providers: [AddressService]
 })
--- api/src/address/address.service.ts
+++ api/src/address/address.service.ts
@@ -3,18 +3,21 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateAddressDto } from './dtos/createAddress.dto';
 import { AddressEntity } from './entities/address.entity';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class AddressService {
     constructor(
         @InjectRepository(AddressEntity)
         private readonly addressRepository: Repository<AddressEntity>,
+        private readonly userService: UserService,
     ) {}
 
     async createAddress(
         createAddressDto: CreateAddressDto,
         userId: number,
       ): Promise<AddressEntity> {
+        await this.userService.findUserById(userId);
         return this.addressRepository.save({
           ...createAddressDto,
           userId,
