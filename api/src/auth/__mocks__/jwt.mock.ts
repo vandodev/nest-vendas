de7aa08e282c540249d68b355253b7972b69b2cf
vandodev <vandoaparecido@hotmail.com>
2024-08-10 08:36:01 -0300

    :computer: :credit_card: :moneybag: Criação do teste unitário da classe AuthService

---
+++ api/src/auth/__mocks__/jwt.mock.ts
@@ -0,0 +1,2 @@
+export const jwtMock =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidHlwZVVzZXIiOjIsImlhdCI6MTcyNTQ5NDIxMywiZXhwIjoxNzI2MDk5MDEzfQ.hIi3rpPl-qc_a-FqW0Wfm39rxuK11EWrhBPLyC65M7I';
