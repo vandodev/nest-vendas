0cfc27006c661dd5e23632a4cea458e8b0fb0077
vandodev <vandoaparecido@hotmail.com>
2024-08-12 07:03:58 -0300

    :computer: :credit_card: :moneybag: Rota para inserir produto

--- api/src/product/product.service.ts
+++ api/src/product/product.service.ts
@@ -2,12 +2,15 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { ProductEntity } from './entities/product.entity';
+import { CreateProductDTO } from './dtos/create-product.dto';
+import { CategoryService } from '../category/category.service';
 
 @Injectable()
 export class ProductService {
   constructor(
     @InjectRepository(ProductEntity)
     private readonly productRepository: Repository<ProductEntity>,
+    private readonly categoryService: CategoryService,
   ) {}
 
   async findAll(): Promise<ProductEntity[]> {
@@ -18,5 +21,13 @@ export class ProductService {
     }
 
     return products;
+  }
+
+  async createProduct(createProduct: CreateProductDTO): Promise<ProductEntity> {
+    await this.categoryService.findCategoryById(createProduct.categoryId);
+
+    return this.productRepository.save({
+      ...createProduct,
+    });
   }
 }
