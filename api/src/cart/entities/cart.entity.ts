91766adba6b05fd65419764c505fe17726692f23
vandodev <vandoaparecido@hotmail.com>
2024-08-14 08:11:15 -0300

    :computer: :credit_card: :moneybag: Criando cart-product entidades e relações

--- api/src/cart/entities/cart.entity.ts
+++ api/src/cart/entities/cart.entity.ts
@@ -1,10 +1,14 @@
 import {
     Column,
     CreateDateColumn,
+    Entity,
+    OneToMany,
     PrimaryGeneratedColumn,
     UpdateDateColumn,
   } from 'typeorm';
+  import { CartProdutEntity } from 'src/cart-product/entities/cart-product.entity';
   
+  @Entity({ name: 'cart' })
   export class CartEntity {
     @PrimaryGeneratedColumn('rowid')
     id: number;
@@ -17,4 +21,7 @@ import {
   
     @UpdateDateColumn({ name: 'updated_at' })
     updatedAt: Date;
+
+    @OneToMany(() => CartProdutEntity, (cartProduct) => cartProduct.cart)
+    cartProduct?: CartProdutEntity[];
   }
