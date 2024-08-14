2c88ecf3e2c52781972924acf67f3020fc85be39
vandodev <vandoaparecido@hotmail.com>
2024-08-14 09:00:55 -0300

    :computer: :credit_card: :moneybag: Inserindo produto no carrinho

--- api/src/cart/cart.service.ts
+++ api/src/cart/cart.service.ts
@@ -3,12 +3,14 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CartEntity } from './entities/cart.entity';
 import { InsertCartDTO } from './dtos/insert-cart.dto';
+import { CartProductService } from 'src/cart-product/cart-product.service';
 
 @Injectable()
 export class CartService {
   constructor(
     @InjectRepository(CartEntity)
     private readonly cartRepository: Repository<CartEntity>,
+    private readonly cartProductService: CartProductService,
   ) {}
 
   async verifyActiveCart(userId: number): Promise<CartEntity> {
@@ -29,14 +31,15 @@ export class CartService {
       userId,
     });
   }
-  
+
   async insertProductInCart(
-    insertCart: InsertCartDTO,
+    insertCartDTO: InsertCartDTO,
     userId: number,
   ): Promise<CartEntity> {
     const cart = await this.verifyActiveCart(userId).catch(async () => {
       return this.createCart(userId);
     });
+    await this.cartProductService.insertProductInCart(insertCartDTO, cart);
     return cart;
   }
 }
