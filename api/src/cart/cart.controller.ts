914e4de9ae54748d6394bed224bcf2485e60fe2b
vandodev <vandoaparecido@hotmail.com>
2024-08-15 07:28:08 -0300

    :computer: :credit_card: :moneybag: Tratando retorno da requisição de carrinho

--- api/src/cart/cart.controller.ts
+++ api/src/cart/cart.controller.ts
@@ -11,7 +11,7 @@ import { UserType } from 'src/user/enum/user-type.enum';
 import { UserId } from 'src/decorators/user-id.decorator';
 import { CartService } from './cart.service';
 import { InsertCartDTO } from './dtos/insert-cart.dto';
-import { CartEntity } from './entities/cart.entity';
+import { ReturnCartDTO } from './dtos/return-cart.dto';
 
 @Roles(UserType.User, UserType.Admin)
 @Controller('cart')
@@ -22,7 +22,9 @@ export class CartController {
     async createCart(
       @Body() insertCart: InsertCartDTO,
       @UserId() userId: number,
-    ): Promise<CartEntity> {
-      return this.cartService.insertProductInCart(insertCart, userId);
+    ): Promise<ReturnCartDTO> {
+      return new ReturnCartDTO(
+        await this.cartService.insertProductInCart(insertCart, userId),
+      );
     }
 }
