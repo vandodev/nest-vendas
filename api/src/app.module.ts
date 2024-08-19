ba4135fed749a3368c893995148ecfcb0105385e
vandodev <vandoaparecido@hotmail.com>
2024-08-19 08:01:25 -0300

    :computer: :credit_card: :moneybag: Criando service e modeule de estatus

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -14,6 +14,7 @@ import { CategoryModule } from './category/category.module';
 import { ProductModule } from './product/product.module';
 import { CartModule } from './cart/cart.module';
 import { CartProductModule } from './cart-product/cart-product.module';
+import { PaymentStatusModule } from './payment-status/payment-status.module';
 
 @Module({
   imports: [
@@ -42,6 +43,7 @@ import { CartProductModule } from './cart-product/cart-product.module';
     ProductModule,
     CartModule,
     CartProductModule,
+    PaymentStatusModule,
   ],
   controllers: [],
   providers: [
