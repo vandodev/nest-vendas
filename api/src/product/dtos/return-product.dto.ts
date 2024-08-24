a1321786142895dd8a2efcb565abace693e90569
vandodev <vandoaparecido@hotmail.com>
2024-08-24 08:45:58 -0300

    :computer: :credit_card: :moneybag: Inserir informações de categorias na busca de produtos

--- api/src/product/dtos/return-product.dto.ts
+++ api/src/product/dtos/return-product.dto.ts
@@ -1,15 +1,20 @@
 import { ProductEntity } from '../entities/product.entity';
+import { ReturnCategory } from 'src/category/dtos/return-category.dto';
 
 export class ReturnProduct {
   id: number;
   name: string;
   price: number;
   image: string;
+  category?: ReturnCategory;
 
   constructor(productEntity: ProductEntity) {
     this.id = productEntity.id;
     this.name = productEntity.name;
     this.price = productEntity.price;
     this.image = productEntity.image;
+    this.category = productEntity.category
+      ? new ReturnCategory(productEntity.category)
+      : undefined;
   }
 }
--- api/src/product/product.controller.ts
+++ api/src/product/product.controller.ts
@@ -25,7 +25,9 @@ export class ProductController {
 
   @Get()
   async findAll(): Promise<ReturnProduct[]> {
-    return (await this.productService.findAll()).map(
+    // return (await this.productService.findAll([1], true)).map(
+    // return (await this.productService.findAll([])).map(
+    return (await this.productService.findAll([], true)).map(
       (product) => new ReturnProduct(product),
     );
   }
