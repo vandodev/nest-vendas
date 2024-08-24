13f89b87500124f2c507f4c909b36e7a2d182a0d
vandodev <vandoaparecido@hotmail.com>
2024-08-24 07:49:03 -0300

    :computer: :credit_card: :moneybag: Teste unitário de order controller

---
+++ api/src/order/__tests__/order.controller.spec.ts
@@ -0,0 +1,39 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { userEntityMock } from '../../user/__mocks__/user.mock';
+import { OrderController } from '../order.controller';
+import { OrderService } from '../order.service';
+import { orderMock } from '../__mocks__/order.mock';
+
+describe('OrderController', () => {
+  let controller: OrderController;
+  let orderService: OrderService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        {
+          provide: OrderService,
+          useValue: {
+            createOrder: jest.fn().mockResolvedValue(orderMock),
+            findOrdersByUserId: jest.fn().mockResolvedValue([orderMock]),
+          },
+        },
+      ],
+      controllers: [OrderController],
+    }).compile();
+
+    controller = module.get<OrderController>(OrderController);
+    orderService = module.get<OrderService>(OrderService);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+    expect(orderService).toBeDefined();
+  });
+
+  it('should return orders in findOrdersByUserId', async () => {
+    const orders = await controller.findOrdersByUserId(userEntityMock.id);
+
+    expect(orders).toEqual([orderMock]);
+  });
+});
