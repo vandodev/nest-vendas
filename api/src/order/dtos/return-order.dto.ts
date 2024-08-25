e6e9e816a2971006382d49f0b215eec26b0b2e0c
vandodev <vandoaparecido@hotmail.com>
2024-08-25 07:20:56 -0300

    :computer: :credit_card: :moneybag: Listagens de pedido feitos pelos usuários

---
+++ api/src/order/dtos/return-order.dto.ts
@@ -0,0 +1,13 @@
+import { ReturnUserDto } from '../../user/dtos/returnUser.dto';
+import { OrderEntity } from '../entities/order.entity';
+
+export class ReturnOrderDTO {
+  id: number;
+  date: string;
+  user?: ReturnUserDto;
+  constructor(order: OrderEntity) {
+    this.id = order.id;
+    this.date = order.date.toString();
+    this.user = order.user ? new ReturnUserDto(order.user) : undefined;
+  }
+}
--- api/src/order/order.controller.ts
+++ api/src/order/order.controller.ts
@@ -10,7 +10,11 @@ import {
   import { OrderService } from './order.service';
   import { UserId } from '../decorators/user-id.decorator';
   import { OrderEntity } from './entities/order.entity';
+  import { Roles } from '../decorators/roles.decorator';
+  import { UserType } from '../user/enum/user-type.enum';
+  import { ReturnOrderDTO } from './dtos/return-order.dto';
   
+  @Roles(UserType.Admin, UserType.User)
   @Controller('order')
   export class OrderController {
     constructor(private readonly orderService: OrderService) {}
@@ -27,5 +31,13 @@ import {
     @Get()
     async findOrdersByUserId(@UserId() userId: number): Promise<OrderEntity[]> {
       return this.orderService.findOrdersByUserId(userId);
+    }
+
+    @Roles(UserType.Admin)
+    @Get('/all')
+    async findAllOrders(): Promise<ReturnOrderDTO[]> {
+      return (await this.orderService.findAllOrders()).map(
+        (order) => new ReturnOrderDTO(order),
+      );
     }
   }
