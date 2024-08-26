48e5f77c5b0cf446ee9b62737f3e03e63424b8f2
vandodev <vandoaparecido@hotmail.com>
2024-08-26 07:36:37 -0300

    :computer: :credit_card: :moneybag: Consulta de CEP utilizando a API dos correios

---
+++ api/src/correios/correios.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { CorreiosService } from './correios.service';
+import { CorreiosController } from './correios.controller';
+import { HttpModule } from '@nestjs/axios';
+
+@Module({
+  imports: [
+    HttpModule.register({
+      timeout: 5000,
+      maxRedirects: 5,
+    }),
+  ],
+  providers: [CorreiosService],
+  controllers: [CorreiosController],
+})
+
+export class CorreiosModule {}
